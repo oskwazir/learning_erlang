75b4e5d03c6ed2d9c94c28de069644f532d658f4
Omer Wazir <omer@thewazir.com>
2015-10-20 11:56:51 -0700

    More guards

--- functions/functions.erl
+++ functions/functions.erl
@@ -1,5 +1,12 @@
 -module(functions).
--export([head/1,second/1,same/2,greet/2, old_enough/1]).
+-export([
+	head/1,
+	second/1,
+	same/2,
+	greet/2,
+	old_enough/1,
+	right_age/1,
+	wrong_age/1]).
 
 head([H|_]) -> H.
 second([_,X|_]) -> X.
@@ -17,3 +24,13 @@ io:format("Hello, ~s!", [Name]).
 
 old_enough(X) when X >= 16 -> true;
 old_enough(_) -> false.
+
+right_age(X) when X >= 16, X =< 104 ->
+true;
+right_age(_) ->
+false.
+
+wrong_age(X) when X < 16; X > 104 ->
+true;
+wrong_age(_) ->
+false.
