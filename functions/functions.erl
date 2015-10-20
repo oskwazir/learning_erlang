5ca688f4b5a89e4919b9c7ede84efa85e9539fc6
Omer Wazir <omer.wazir@gmail.com>
2015-10-19 22:31:38 -0700

    Add another pattern matching example

--- functions/functions.erl
+++ functions/functions.erl
@@ -7,3 +7,10 @@ same(X,X) ->
 	true;
 same(_,_) ->
 	false. 
+
+greet(male,Name) ->
+io:format("Hello, Mr. ~s!", [Name]);
+greet(female,Name) ->
+io:format("Hello, Ms. ~s!", [Name]);
+greet(_,Name) ->
+io:format("Hello, ~s!", [Name]).
