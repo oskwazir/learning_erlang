bcc7c4b5b8e931a113eb835a4d0d3815e8418ea8
Omer Wazir <omer.wazir@gmail.com>
2015-11-26 14:36:03 -0700

    Document the module

--- etudeds_for_erlang/geom/geom.erl
+++ etudeds_for_erlang/geom/geom.erl
@@ -1,5 +1,12 @@
+%% @author Omer Wazir <omer@thewazir.com>
+%% @doc Functions for calculating areas of geometric shapes.
+%% @version 0.1
+
 -module(geom).
 -export([area/2]).
 
-% Take length and width and return area
+%  @doc Take length and width of rectangle and return area
+
+-spec(area(number(),number()) -> number()).
+
 area(Length,Width) -> Length * Width.
