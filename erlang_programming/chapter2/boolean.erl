6865a8ebee42f95b04efdaafe5d314328cb17d08
Omer Wazir <omer.wazir@gmail.com>
2015-11-30 18:17:11 -0700

    Remove defense patterns

--- erlang_programming/chapter2/boolean.erl
+++ erlang_programming/chapter2/boolean.erl
@@ -11,18 +11,15 @@
 
 b_not(false) -> true;
 b_not(true) -> false;
-b_not(_) -> {error, invalid_object}.
 
 b_and(true,true) -> true; 
 b_and(false,true) -> false;
 b_and(true,false) -> false;
 b_and(false,false) -> false;
-b_and(_,_) -> {error, invalid_object}.
 
 b_or(true,true) -> true; 
 b_or(false,true) -> true;
 b_or(true,false) -> true;
 b_or(false,false) -> false;
-b_or(_,_) -> {error, invalid_object}.
 
 b_nand(A,B) -> b_not(b_and(A,B)).
