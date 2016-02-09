ec2d7321c51127206cd5c450c0d306271e61018c
Omer Wazir <omer@thewazir.com>
2016-02-09 16:19:41 -0700

    Going over recursion

---
+++ useless/recursion.erl
@@ -0,0 +1,28 @@
+-module(recursion).
+-export([
+	bump/1,
+	average/1, 
+	even/1,
+	member/2]).
+
+bump([]) -> [];
+bump([Head | Tail]) ->
+	[Head + 1 | bump(Tail)].
+
+average(List) -> sum(List) / len(List).
+
+sum([]) -> 0;
+sum([Head | Tail]) ->
+	Head + sum(Tail).
+
+len([]) -> 0;
+len([_ | Tail]) -> 1 + len(Tail).
+
+even([]) -> [];
+even([Head | Tail]) when Head rem 2 == 0 ->
+	[Head | even(Tail)];
+even([_ | Tail]) -> even(Tail).
+
+member(_,[]) -> false;
+member(H, [H | _]) -> true;
+member(H, [_ | T]) -> member(H, T).
