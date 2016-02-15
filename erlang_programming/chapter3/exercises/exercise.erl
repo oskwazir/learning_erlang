439afb4d0c60556ff798b9ebd4efb1909e08d3fb
Omer Wazir <omer@thewazir.com>
2016-02-15 15:04:40 -0700

    Finish Exercise 3-2

--- erlang_programming/chapter3/exercises/exercise.erl
+++ erlang_programming/chapter3/exercises/exercise.erl
@@ -1,5 +1,8 @@
 -module(exercise).
--export([sum/1]).
+-export([sum/1,
+		 create/1,
+		 reverse_create/1]).
+-import(lists, [reverse/1]).
 
 sum_acc(N,Sum) when N < 1 -> Sum;
 sum_acc(N,Sum) ->
@@ -7,3 +10,10 @@ sum_acc(N,Sum) ->
 
 sum(N) ->
 	sum_acc(N,0).
+
+create_acc(N,L) when N < 1 -> L;
+create_acc(N,L) ->
+	create_acc(N - 1, [ N | L ]).
+
+create(N) -> create_acc(N,[]).
+reverse_create(N) -> lists:reverse(create(N)).
