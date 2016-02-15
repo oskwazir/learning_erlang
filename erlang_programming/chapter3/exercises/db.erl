c5d76a9c13cb6cccaaf69bf62abddcff5bdd75f5
Omer Wazir <omer@thewazir.com>
2016-02-15 23:32:29 +0000

    Add new file to ignore

--- erlang_programming/chapter3/exercises/db.erl
+++ erlang_programming/chapter3/exercises/db.erl
@@ -8,6 +8,12 @@
 	match/2
 	]).
 
+reverse_acc([], Reversed) -> Reversed;
+reverse_acc([ Head | Tail ], Reversed) ->
+	reverse_acc(Tail, [ Head | Reversed ]).
+
+reverse(List) ->
+	reverse_acc(List,[]).
 
 new() -> [].
 destroy(_) -> [].
@@ -21,8 +27,17 @@ read(_,[]) ->
 	{error, instance};
 read(Search, [ {Key, Value} | _ ])
 	when Search == Key ->
-	{ok, Value};
+		{ok, Value};
 read(Search, [ _ | Tail]) ->
 	read(Search,Tail).
 
-match(_,_)  -> [].
+match_acc(_,[],Results) -> Results;
+match_acc(Search,[ {Key, Value} | Tail ], Results)
+	when Search == Value ->
+		match_acc(Search,Tail, [ Key | Results ]);
+match_acc(Search,[ _ | Tail ], Results) ->
+	match_acc(Search, Tail, Results ).
+
+match(_,[])  -> [];
+match(Search,Db) ->
+	reverse(match_acc(Search,Db,[])).
