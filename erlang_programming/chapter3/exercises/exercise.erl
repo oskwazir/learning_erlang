903f0693adecbff9591a2b2062f2d578610e48dc
Omer Wazir <omer@thewazir.com>
2016-02-15 15:18:03 -0700

    Exercise 3-3 complete

--- erlang_programming/chapter3/exercises/exercise.erl
+++ erlang_programming/chapter3/exercises/exercise.erl
@@ -1,7 +1,9 @@
 -module(exercise).
 -export([sum/1,
 		 create/1,
-		 reverse_create/1]).
+		 reverse_create/1,
+		 between/1,
+		 between_even/1]).
 -import(lists, [reverse/1]).
 
 sum_acc(N,Sum) when N < 1 -> Sum;
@@ -17,3 +19,25 @@ create_acc(N,L) ->
 
 create(N) -> create_acc(N,[]).
 reverse_create(N) -> lists:reverse(create(N)).
+
+between_acc(N, _) when N < 1 -> 
+	io:format("");
+between_acc(N, Sum) ->
+	io:format("Number: ~p~n", [Sum]),
+	between_acc(N - 1, Sum + 1).
+
+between(N) ->
+	between_acc(N, 1).
+
+
+between_even_acc(N, _) when N < 1 ->
+	io:format("");
+between_even_acc(N, Sum) when Sum rem 2 == 0 ->
+	io:format("Number: ~p~n", [Sum]),
+	between_even_acc(N - 1, Sum + 1);
+between_even_acc(N,Sum) ->
+	between_even_acc(N - 1, Sum + 1).
+
+between_even(N) ->
+	between_even_acc(N, 1).
+
