64a7d28043cb0228a19216f138da002313058b5a
Omer Wazir <omer@thewazir.com>
2016-02-15 15:25:10 -0700

    Define Database interface.

---
+++ erlang_programming/chapter3/exercises/db.erl
@@ -0,0 +1,16 @@
+-module(db).
+-export([
+	new/0,
+	destroy/1,
+	write/3,
+	delete/2,
+	read/2,
+	match/2
+	]).
+
+new() -> [].
+destroy(_) -> [].
+write(_,_,_)  -> [].
+delete(_,_)  -> [].
+read(_,_)  -> [].
+match(_,_)  -> [].
