8657315fc648398fbf1aefe9a9734f2b7af3cca9
Omer Wazir <omer@thewazir.com>
2016-05-26 10:14:54 -0700

    Abstract messages into functions

--- kitchen/kitchen.erl
+++ kitchen/kitchen.erl
@@ -1,33 +1,33 @@
 -module(kitchen).
--compile(export_all).
+-export([store/2,take/2,fridge/1]).
 
-fridge1() ->
-	receive
-		{From, {store, _Food}} ->
-			From ! {self(), ok},
-			fridge1();
-		{From, {take, _Food}} ->
-			%% uh....
-			From ! {self(), not_found},
-			fridge1();
-		terminate ->
-			ok
-	end.
-
-fridge2(FoodList) ->
+fridge(FoodList) ->
 	receive
 		{From, {store, Food}} ->
 			From ! {self(), ok},
-			fridge2([Food|FoodList]);
+			fridge([Food|FoodList]);
 		{From, {take, Food}} ->
 			case lists:member(Food, FoodList) of
 				true ->
 					From ! {self(), {ok, Food}},
-					fridge2(lists:delete(Food, FoodList));
+					fridge(lists:delete(Food, FoodList));
 				false ->
 					From ! {self(), not_found},
-					fridge2(FoodList)
+					fridge(FoodList)
 			end;
 		terminate ->
 			ok
 	end.
+
+store(Pid, Food) ->
+	Pid ! {self(), {store,Food}},
+	receive
+		{Pid, Msg} -> Msg
+	end.
+
+take(Pid, Food) ->
+	Pid ! {self(), {take, Food}},
+	receive
+		{Pid,Msg} -> Msg
+	end.
+
